d073af73280c71b0656502eb9af646c2e5785120
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-03-24 09:52:11 +0200

    q2

--- G-NET-27-OOP-6/Program.cs
+++ G-NET-27-OOP-6/Program.cs
@@ -16,6 +16,14 @@ namespace G_NET_27_OOP_6
             // make every employee what he do but hide how do it => abstract 
 
             #endregion
+
+            #region Q2
+            // abstract => 1- can have abstract method 2-can have constructor 3- can have fields 4- can have access modifier 5- cant have multiple inheritance 
+            // interface => 1- can not have abstract method 2-can not have constructor 3- can not have fields 4- can not have access modifier 5- cant have multiple inheritance only have method signature and properties signature
+            // choose abstract if i have 1- common logic between classes 2- i want to share code 3- classes are closely related 
+            // choose interface if i 1- want to define role 2- want multiaple inhertance
+            // 
+            #endregion
             #endregion
         }
     }
