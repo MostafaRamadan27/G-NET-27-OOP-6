746e805cd6ee045ae59696bfec1d27ae4c3a1fae
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-03-24 09:42:02 +0200

    Q1

--- G-NET-27-OOP-6/Program.cs
+++ G-NET-27-OOP-6/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace G_NET_27_OOP_6
+﻿using System.ComponentModel;
+
+namespace G_NET_27_OOP_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 : Theoretical Questions
+
+            #region Q1
+            // Abstract class is process of exposing only what user needs and hide how it is implemented
+            // encapsulation is procees of hiding data and only exposing neccesary data to user and abstract is process 0f hiding implements and only exposing what user needs
+            // example Employee 
+            // can make salary private and cant change it directly make method to get salary => encapsulation
+            // make every employee what he do but hide how do it => abstract 
+
+            #endregion
+            #endregion
         }
     }
 }
